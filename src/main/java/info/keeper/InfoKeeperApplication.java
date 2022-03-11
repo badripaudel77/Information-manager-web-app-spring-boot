078add756ffc99b55d2234ef10b7443cafe029d7
badripaudel77 <badripaudel77@gmail.com>
2022-03-11 15:14:06 +0545

    logger added slf4j

--- src/main/java/info/keeper/InfoKeeperApplication.java
+++ src/main/java/info/keeper/InfoKeeperApplication.java
@@ -1,13 +1,16 @@
 package info.keeper;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InfoKeeperApplication {
 	public static void main(String[] args) {
-		System.out.println(">>application starting in localhost[ Triggering Jenkins Build]");
+		Logger logger = LoggerFactory.getLogger(InfoKeeperApplication.class);
+		logger.debug(">>application starting in localhost[ Triggering Jenkins Build]");
 		SpringApplication.run(InfoKeeperApplication.class, args);
-		System.out.println("<<application started fine in localhost [without docker] [Triggering Jenkins Build]");
+		logger.debug("<<application started fine in localhost [without docker] [Triggering Jenkins Build]");
 	}
 }
