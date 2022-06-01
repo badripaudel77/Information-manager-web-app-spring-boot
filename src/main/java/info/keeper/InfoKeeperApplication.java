6a007091b1688cf5a4007f511993f633ea699e20
badripaudel77 <badri@eventlogic.se>
2022-06-01 14:24:38 +0545

    DB url updated, log changed.

--- src/main/java/info/keeper/InfoKeeperApplication.java
+++ src/main/java/info/keeper/InfoKeeperApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InfoKeeperApplication {
 	public static void main(String[] args) {
-		System.out.println(">>application starting with docker-compose");
+		System.out.println(">>application starting");
 		SpringApplication.run(InfoKeeperApplication.class, args);
-		System.out.println("<<application started with docker-compose fine");
+		System.out.println("<<application started");
 	}
 }
