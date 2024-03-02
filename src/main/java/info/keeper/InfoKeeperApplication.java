99fa88830f831c75bf4108765741ddd4368e600a
Badri Paudel <badripaudel77@gmail.com>
2024-03-02 22:54:07 +0545

    logger added for app running.

--- src/main/java/info/keeper/InfoKeeperApplication.java
+++ src/main/java/info/keeper/InfoKeeperApplication.java
@@ -1,11 +1,15 @@
 package info.keeper;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InfoKeeperApplication {
+	static Logger logger = LoggerFactory.getLogger(InfoKeeperApplication.class);
 	public static void main(String[] args) {
+		logger.info("Information app is running ");
 		SpringApplication.run(InfoKeeperApplication.class, args);
 	}
 }
